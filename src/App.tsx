fc91670d496be25b37800668471688b0ccf348c9
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-08-26 20:31:59 +0200

    Footer and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.scss";
+import Button from "./components/button/Button";
 import Header from "./components/header/Header";
-import Price from "./components/sections/Price";
-import Whatsthat from "./components/sections/Whatsthat";
 
 {
 	/* <a href="https://vitejs.dev" target="_blank">
@@ -13,8 +12,39 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Price />
-			<Whatsthat />
+			<main>
+				<section className="price">
+					<h2>Price</h2>
+					<div className="price__desc">
+						<span className="bold font__size-regular-large">
+							<sup>$</sup>0
+						</span>
+						<span className="space"> /</span>
+						month
+					</div>
+				</section>
+				<section className="what">
+					<h2>What is it?</h2>
+					<ul className="what__list">
+						<li>no ads</li>
+						<li>free</li>
+						<li>customizable</li>
+						<li>A4 template</li>
+						<li>you can generate</li>
+						<li>print</li>
+						<li>and use</li>
+					</ul>
+					<div className="what__examples">
+						You can use it as: {""}
+						<span className="gray">
+							training plan, skin care plan, diet plan and many more
+						</span>
+					</div>
+					<div className="what__interested bold">Interested?</div>
+					<Button text="START" border="white" />
+				</section>
+			</main>
+			<footer className="footer">@{new Date().getFullYear()}</footer>
 		</div>
 	);
 }
